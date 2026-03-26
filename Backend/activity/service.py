5b52cf050353fc3564ec70dea79f8b37c7a489ae
Rdxsandy <sundeepgangwar@gmail.com>
2026-03-26 11:53:18 +0530

    change in dashboard code and activity

--- Backend/activity/service.py
+++ Backend/activity/service.py
@@ -7,11 +7,32 @@ activity_collection = db["activities"]
 from datetime import datetime
 
 # CREATE ACTIVITY
+from datetime import datetime
+
 def create_activity(activity_data: dict):
 
-    # Ensure date exists
-    if "date" not in activity_data:
-        activity_data["date"] = datetime.utcnow()
+    today_start = datetime.utcnow().replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+
+    existing = activity_collection.find_one({
+        "user_id": activity_data["user_id"],
+        "date": {"$gte": today_start}
+    })
+
+    # If activity exists for today → update
+    if existing:
+
+        activity_collection.update_one(
+            {"_id": existing["_id"]},
+            {"$set": activity_data}
+        )
+
+        activity_data["_id"] = str(existing["_id"])
+        return activity_data
+
+    # Otherwise insert new record
+    activity_data["date"] = datetime.utcnow()
 
     result = activity_collection.insert_one(activity_data)
 
